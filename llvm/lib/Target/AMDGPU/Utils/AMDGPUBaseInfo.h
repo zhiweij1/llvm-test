790c596421e5f1f8b65ed3f713b3a8a3f4403e74
Diana Picus <diana-magda.picus@amd.com>
2025-06-05 13:05:45 +0200

    [AMDGPU] Tail call support for whole wave functions
    Support tail calls to whole wave functions (trivial) and from whole wave
    functions (slightly more involved because we need a new pseudo for the
    tail call return, that patches up the EXEC mask).
    
    Move the expansion of whole wave function return pseudos (regular and
    tail call returns) to prolog epilog insertion, since that's where we
    patch up the EXEC mask.
    
    Unnecessary register spills will be dealt with in a future patch.

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
@@ -1499,6 +1499,7 @@ constexpr bool mayTailCallThisCC(CallingConv::ID CC) {
   switch (CC) {
   case CallingConv::C:
   case CallingConv::AMDGPU_Gfx:
+  case CallingConv::AMDGPU_Gfx_WholeWave:
     return true;
   default:
     return canGuaranteeTCO(CC);
